72cca364b9e4eb51cab444b0d4ea480b10eeae54
Ivan Vodopianov <vis3010@gmail.com>
2022-08-26 00:31:11 +0300

    start HW-7 on JS

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+galleryContainer.innerHTML = creatElemens(galleryItems);
+
+
+
+function creatElemens(gallery) {
+    const imageEl = gallery.map(({preview, original, description}) => 
+    `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`).join('');
+
+  return imageEl
+}
