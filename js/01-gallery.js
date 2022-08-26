485da8da585477b65f1caacbbb8f3e5d9342a3ee
Ivan Vodopianov <vis3010@gmail.com>
2022-08-26 16:04:27 +0300

    end task-1 HW-7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -7,20 +7,60 @@ const galleryContainer = document.querySelector('.gallery');
 
 galleryContainer.innerHTML = creatElemens(galleryItems);
 
-
-
 function creatElemens(gallery) {
-    const imageEl = gallery.map(({preview, original, description}) => 
-    `<div class="gallery__item">
+  const imageEl = gallery
+    .map(
+      ({ preview, original, description }) =>
+        `<div class="gallery__item">
     <a class="gallery__link" href="${original}">
-      <img
-        class="gallery__image"
-        src="${preview}"
-        data-source="${original}"
-        alt="${description}"
-      />
+    <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+    />
     </a>
-  </div>`).join('');
+    </div>`
+    )
+    .join('');
 
-  return imageEl
+  return imageEl;
 }
+
+let modalWindow = null;
+
+function showModal() {
+  const showModalLightbox = ({ alt, dataset: { source } }) => {
+    modalWindow = basicLightbox.create(
+      `<img style="color: #fff" src="${source}" alt="${alt}" width="800" height="600">`,
+      {
+        onShow: addKeyboardControl,
+        onClose: removeKeyboardControl,
+      }
+    );
+
+    modalWindow.show();
+  };
+
+  const addKeyboardControl = () => window.addEventListener('keydown', onWindowKeyDown);
+  const removeKeyboardControl = () => window.removeEventListener('keydown', onWindowKeyDown);
+
+  const onWindowKeyDown = ({ code }) => {
+    if (code !== 'Escape') return;
+
+    modalWindow.close();
+  };
+
+  const onGalleryContainerClick = event => {
+    event.preventDefault();
+
+    if (event.target.nodeName !== 'IMG') return;
+
+    showModalLightbox(event.target);
+  };
+
+  galleryContainer.addEventListener('click', onGalleryContainerClick);
+}
+
+showModal();
+
